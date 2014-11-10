3493f9b7a973d7cc2b7c2439c030a54e1ac18a51
Johannes Donath <johannesd@evil-co.com>
2014-11-10 22:52:48 +0100

    Added an abstract registry implementation.

---
+++ src/main/java/org/evilco/netty/msgpack/registry/AbstractMessageRegistry.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2014 Johannes Donath <johannesd@evil-co.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.evilco.netty.msgpack.registry;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import lombok.NonNull;
+import org.evilco.netty.msgpack.error.MessageRegistryException;
+import org.evilco.netty.msgpack.error.UnknownMessageException;
+
+/**
+ * Provides a basic packet registry implementation.
+ * @author Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.com>
+ */
+public abstract class AbstractMessageRegistry<T extends Object> implements IMessageRegistry<T> {
+
+	/**
+	 * Stores the registry.
+	 */
+	private BiMap<Short, Class<T>> registry = HashBiMap.create ();
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public short getMessageID (@NonNull Class<T> messageType) throws MessageRegistryException {
+		// verify existence
+		if (this.registry.inverse ().containsKey (messageType)) throw new UnknownMessageException ("Could not find registration for type " + messageType.getName ());
+
+		// get registered identifier
+		return this.registry.inverse ().get (messageType);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Class<T> getMessageType (short messageID) throws MessageRegistryException {
+		// verify existence
+		if (this.registry.containsKey (messageID)) throw new UnknownMessageException ("Could not find registration for identifier " + messageID);
+
+		// get registered type
+		return this.registry.get (messageID);
+	}
+}
